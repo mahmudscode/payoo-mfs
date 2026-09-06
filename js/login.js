12c511493ca05240d50070f104812750ca68a3dc
AK2b_9yMgt*9uKj-m <mahmud102496@gmail.com>
2026-09-06 19:52:28 +0600

    login-js1

---
+++ js/login.js
@@ -0,0 +1,10 @@
+// console.log("login.js loaded");
+
+document.getElementById('loginBtn').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+   console.log("Login button clicked");
+
+   const phonenumber = document.getElementById('ph-num').value;
+    console.log(phonenumber);
+}
+)
